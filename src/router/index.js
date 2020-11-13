6634bd4bfe425d9392854da64d735880d2d51dec
VALKERIZ <959749198@qq.com>
2020-11-13 23:50:36 +0800

    初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import index from "../views/index";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
+const routes = [{
     path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
+    name: "index",
+    component: index
+}, ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
 });
 
 export default router;
---
+++ src/views/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="amap-wrap">
+    <el-amap vid="amapDemo" :zoom="zoom" class="amap-demo"> </el-amap>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {
+      zoom: 12,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.amap-wrap {
+  height: 100vh;
+}
+</style>
